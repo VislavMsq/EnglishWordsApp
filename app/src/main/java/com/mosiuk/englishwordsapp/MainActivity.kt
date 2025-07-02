21a2c94a4cd370bd5611d04f38c7e426a0e8dd7b
vladislavmosuyk <tokenvlad@gmail.com>
2025-07-02 21:25:06 +0200

    add ViewBinding

--- app/src/main/java/com/mosiuk/englishwordsapp/MainActivity.kt
+++ app/src/main/java/com/mosiuk/englishwordsapp/MainActivity.kt
@@ -1,14 +1,39 @@
 package com.mosiuk.englishwordsapp
 
+import android.graphics.Color
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.isVisible
+import com.mosiuk.englishwordsapp.databinding.ActivityLearnWordBinding
 
 class MainActivity : AppCompatActivity() {
+
+//    private lateinit var binding: ActivityLearnWordBinding
+
+    private var _binding: ActivityLearnWordBinding? = null
+    private val binding
+        get() = _binding ?: throw IllegalStateException("Binding for ActivityLearnWordBinding must not be null")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_learn_word)
+        _binding = ActivityLearnWordBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // findViewById
+        // viewBinding
+
+//        val tvQuestionWord: TextView = findViewById(R.id.tvQuestionWord)
+//
+//        tvQuestionWord.text = "666"
+//        tvQuestionWord.setTextColor(Color.RED)
+//        tvQuestionWord.setTextColor(Color.parseColor("#FDD600"))
+//        tvQuestionWord.setTextColor(ContextCompat.getColor(this, R.color.buttonBlueColor))
+
+        with(binding) {
+            tvQuestionWord.text = "EnglishWordsApp"
+            tvQuestionWord.setTextColor(Color.RED)
+            tvQuestionWord.isVisible = true
+
+        }
     }
 }
